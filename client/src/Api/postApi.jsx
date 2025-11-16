582c59dc915c322b6b329db7afcade41a7f5cec5
Sikandar Kumar <sikandarsahilkumar@example.com>
2025-11-16 21:07:38 +0530

    changes

--- client/src/Api/postApi.jsx
+++
@@ -1,9 +0,0 @@
-import axiosInstance from "./axiosInstance";
-
-export const createPostROute = (user) => {
-  return axiosInstance.post("/api/post/createPost", user);
-};
-
-export const getPosts = () => {
-  return axiosInstance.get("/api/post/getPosts");
-};
