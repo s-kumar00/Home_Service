91cee9ea43dd07097c6734bf574a76e6ee790205
Sikandar Kumar <sikandarsahilkumar@example.com>
2025-11-16 20:53:46 +0530

    removed server part

--- Server/router/post.router.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const postController = require("../controllers/post.controller");
-
-router.post("/createPost", postController.createPost);
-router.get("/getPosts", postController.getPosts);
-module.exports = router;
